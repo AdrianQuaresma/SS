be42f8727e2179edea18c8d09e376e8ecf6bad22
Goncalo <goncalo_louro@hotmail.com>
2017-11-14 15:46:37 +0000

    added new slices, chaged analyser

--- SSof_Proj/src/main/Analyser.java
+++ SSof_Proj/src/main/Analyser.java
@@ -24,76 +24,78 @@ public class Analyser{
 			System.out.println(filename);
 		}
 		
+		JSONObject jsonObject=null;
+		Object obj = null;
+		
+		try{
+			JSONParser parser = new JSONParser();
+			obj = parser.parse(new FileReader("src/resources/" + filename));
+	        
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
 		//load patterns
 		
 		//parse slice
-		
-		checkEntryPoints(filename);
-		
 		//load threats
+		jsonObject =  (JSONObject) obj;
+		checkEntryPoints(filename, jsonObject);
 		
 		//list threats
-		
+		System.out.println("The following variables were identified as threats");
 		for(Threat t: threats){
+			
 			System.out.println(t.getName());
+			
+			//mark variable as sanitized or not
 		}
 		
-		//check for entry points - assignments with entry points from patterns 
 		
+		
+		
+		//check if variable is assigned to another variable, if it is replace names (u->q)?
+		
+		//check if variable is used in sink
 		
 		System.out.println("Analysis done");
 	}
 
-	private static void checkEntryPoints(String filename) {
-		JSONParser parser = new JSONParser();
-		 
-        try {
- 
-            Object obj = parser.parse(new FileReader(
-                    "src/resources/" + filename));
- 
-            JSONObject jsonObject =  (JSONObject) obj;
+	private static void checkEntryPoints(String filename, JSONObject jsonObject) {
             
-            String name;
-            
-            JSONArray children = (JSONArray) jsonObject.get("children");
-            
-            Iterator<JSONObject> iterator = children.iterator();
-            while (iterator.hasNext()) {
-            	jsonObject = iterator.next();
-            	name = (String) jsonObject.get("kind");
-            	System.out.println(name);
-            	
-            	if(name.equals("assign")){
-            		//check right for entry points
-            		JSONObject jsobj = (JSONObject) jsonObject.get("right");
-            		name = (String) jsonObject.get("kind");
-            		if((jsobj).containsKey("what")){
+        String name;
+        
+        JSONArray children = (JSONArray) jsonObject.get("children");
+        
+        Iterator<JSONObject> iterator = children.iterator();
+        while (iterator.hasNext()) {
+        	
+        	jsonObject = iterator.next();
+        	name = (String) jsonObject.get("kind");
+        	
+        	if(name.equals("assign")){
+        		//check right for entry points
+        		JSONObject jsobj = (JSONObject) jsonObject.get("right");
+        		name = (String) jsonObject.get("kind");
+        		if((jsobj).containsKey("what")){
+        			
+            		JSONObject jsobj2 = (JSONObject) jsobj.get("what");
+            		if((jsobj2).containsKey("name")){
             			
-	            		JSONObject jsobj2 = (JSONObject) jsobj.get("what");
-	            		if((jsobj2).containsKey("name")){
-	         			
-	            			if(jsobj2.get("name").equals("_GET")){
-	            				System.out.println("bue fixe");
-	            				
-	            				//check left for variable name
-	            				jsobj = (JSONObject) jsonObject.get("left");
-	            				Threat t = new Threat((String) jsobj.get("name"), false, false);
-	            				threats.add(t);
-	            				
-	            			}
-	            		}
+            			//check for entry points - assignments with entry points from patterns 
+            			if(jsobj2.get("name").equals("_GET") || jsobj2.get("name").equals("_POST")){			
+            				//check left for variable name
+            				jsobj = (JSONObject) jsonObject.get("left");
+            				Threat t = new Threat((String) jsobj.get("name"), false, false);
+            				threats.add(t);
+            				
+            			}
             		}
-                    
-            	}       	
-               // System.out.println(jsonObject);
-            }
-            
-            
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-		
+        		}
+                
+        	}       	
+           // System.out.println(jsonObject);
+        }		
 	}
 	
 	
