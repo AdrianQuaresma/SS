dd219e56200818a3e3720501a006bb263ff67e46
Goncalo <goncalo_louro@hotmail.com>
2017-11-17 00:34:13 +0000

    updated things

--- SSof_Proj/src/main/Analyser.java
+++ SSof_Proj/src/main/Analyser.java
@@ -48,13 +48,15 @@ public class Analyser{
 		
 		
 		//list threats
+		//parse threats
 		System.out.println("The following variables were identified as threats");
 		for(Threat t: threats){
-			
 			System.out.println(t.getName());
+			checkVulnerability(t,filename, jsonObject);
 			
-			//TODO mark variable as sanitized or not
+			
 		}
+		//TODO mark variable as sanitized or not
 		
 		//TODO check if variable is assigned to another variable, if it is, replace names? (u->q)?
 		
@@ -63,6 +65,47 @@ public class Analyser{
 				//if not, print vulnerability
 		
 		System.out.println("Analysis done");
+	}
+
+	private static void checkVulnerability(Threat t,String filename, JSONObject jsonObject) {
+		String name, kind;
+        
+        JSONArray children = (JSONArray) jsonObject.get("children");
+        
+        Iterator<JSONObject> iterator = children.iterator();
+        while (iterator.hasNext()) {
+        	
+        	jsonObject = iterator.next();
+        	name = (String) jsonObject.get("kind");
+        	JSONObject jsobj, jsobj2;
+        	//check if the threat t was assigned to any other variable
+        	if(name.equals("assign")){
+        		jsobj = (JSONObject) jsonObject.get("left");
+        		if(jsobj.get("name").equals(t.getName())){
+        			continue;
+        		}
+        		
+        		jsobj = (JSONObject) jsonObject.get("right");
+        		
+        		kind = (String) jsobj.get("kind");
+        		if(kind.equals("encapsed")){
+        			JSONArray values = (JSONArray) jsonObject.get("value");
+        			Iterator<JSONObject> iterator2 = values.iterator();
+        			while(iterator2.hasNext()){
+        				jsobj2=iterator2.next();
+        			}
+        		}
+        		
+        		
+        		jsobj2 = (JSONObject) jsobj.get("kind");
+        		
+        		
+        		
+        	}
+        	//check if the threat t is sanitized
+        	
+        	//check if the threat t is being used on a sensitive sink
+        }
 	}
 
 	private static void checkEntryPoints(String filename, JSONObject jsonObject) {
@@ -80,16 +123,17 @@ public class Analyser{
         	if(name.equals("assign")){
         		//check right for entry points
         		JSONObject jsobj = (JSONObject) jsonObject.get("right");
-        		name = (String) jsonObject.get("kind");
-        		if((jsobj).containsKey("what")){
+        		name = (String) jsobj.get("kind");
+        		if(name.equals("offsetlookup")){
         			
             		JSONObject jsobj2 = (JSONObject) jsobj.get("what");
             		if((jsobj2).containsKey("name")){
-            			
+  
             			//check for entry points - assignments with entry points from patterns 
             			for(Pattern p : patterns){
             				
             				if(p.getEntrypoints().contains(jsobj2.get("name"))){
+   
 	            				//check left for variable name
 	            				jsobj = (JSONObject) jsonObject.get("left");
 	            				Threat t = new Threat((String) jsobj.get("name"), false, false);
